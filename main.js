61347ca56747700296d1d2900b9f81801c706d57
Salvador Amba Jr <46850774+Buko-pie@users.noreply.github.com>
2024-04-13 21:25:46 +0800

    updates

--- main.js
+++ main.js
@@ -43,4 +43,31 @@ const closeNav = () => {
   menuBtn.style.display = "inline-block";
 };
 
+function formSubmitBtn($event) {
+  /**
+   * Checks the validity of the form.
+   * Return if invalid; HTML5 validation errors should display.
+   */
+  if (!$event.target.form.checkValidity()) {
+      return;
+  }
+  /**
+   * Form is client-side valid; taking over the remainder of processing.
+   */
+  $event.preventDefault();
+  grecaptcha.ready(function() {
+      grecaptcha.execute("6LcYuLkpAAAAAGWDdPvO0UwzXsEsICHqkniMD4-y", { action: 'submit' }).then(function(token) {
+          /**
+           * Adds the token g-recaptcha-response token to our hidden form element.
+           * ** Notice ** we our referencing the specific form's input element by name here (do not use IDs).
+           */
+          $event.target.form.elements['g-recaptcha-response'].value = token;
+          /**
+           * Use the form API directly to submit the form.
+           */
+          $event.target.form.submit();
+      });
+  });
+}
+
 closeBtn.addEventListener("click", closeNav);
